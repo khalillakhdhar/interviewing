f0b6b1d9270ff09542019dc835212aa6869477c9
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-29 10:20:14 +0100

    Revert "Update notifications.component.ts"
    This reverts commit 454e4629a487b5318d1096f5a976e90b19930206.

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -107,17 +107,6 @@ this.produit=new Produit();
 
 
 }
-update()
-{
-  this.produit.photo = this.fb;
-
-  let pr=Object.assign({},this.produit);
-this.produitService.update_Produit(this.produit.id,pr);
-alert("Modifié");
-this.produit=new Produit();
-
-
-}
 readcats()
 {
   this.cateogrieService.read_Categories().subscribe(data => {
